c5c32720f2b3da4bb9ff804248b6e28269b846f6
Alexandr Kashevarov <landing.page.creature@gmail.com>
2021-07-30 21:14:05 +0500

    изменил страницу витрины

--- src/main/java/com/amr/project/webapp/controller/ShowcaseController.java
+++ src/main/java/com/amr/project/webapp/controller/ShowcaseController.java
@@ -1,20 +1,22 @@
 package com.amr.project.webapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/*контроллер страницы витрины магазина*/
 @Controller
 @RequestMapping("/showcase")
 public class ShowcaseController {
     public ShowcaseController() {}
 
+    /*Далее будет использоваться path variable*/
     @GetMapping
-    public String getShowcaseView() {
+    public String getShowcaseView(Model model) {
+        model.addAttribute("market_id", "1");
+        model.addAttribute("market_name", "Ozon (test)");
         return "showcase";
     }
-    @GetMapping("/full")
-    public String getFullShowcaseView() {
-        return "fullShowcase";
-    }
+
 }
---
+++ src/main/java/com/amr/project/webapp/rest_controller/ShowcaseRestController.java
@@ -0,0 +1,11 @@
+package com.amr.project.webapp.rest_controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*Rest контроллер для работы с товарами и описанием магазина*/
+@RestController
+@RequestMapping("/showcase/{id}")
+public class ShowcaseRestController {
+
+}
