e7e167f74d2f989b195a1363bb0898a585049ce7
Alexandr Martynets <amartinets@yandex.ru>
2021-08-02 18:34:03 +0300

    правки + рефактор

--- src/main/java/com/amr/project/dao/impl/UserDAOimpl.java
+++ src/main/java/com/amr/project/dao/impl/UserDAOimpl.java
@@ -1,9 +1,19 @@
 package com.amr.project.dao.impl;
 
-import com.amr.project.dao.abstracts.UserDAO;
+import com.amr.project.dao.abstracts.UserDao;
 import com.amr.project.model.entity.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public class UserDAOimpl extends ReadWriteDAOImpl<User, Long> implements UserDAO {
+public class UserDAOImpl extends ReadWriteDAOImpl<User, Long> implements UserDao {
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return entityManager.createQuery("SELECT u from User u where u.username = :username", User.class)
+                .setParameter("username", username).getResultList()
+                .stream()
+                .findAny();
+
+    }
 }
