2bf8042788801336cf6856043a4de9fe66945fbe
Nadin <programmer_n@bk.ru>
2021-08-03 15:34:47 +0500

    исправила DAO, Servise + import.sql

--- src/main/java/com/amr/project/service/abstracts/ItemService.java
+++ src/main/java/com/amr/project/service/abstracts/ItemService.java
@@ -3,5 +3,24 @@ package com.amr.project.service.abstracts;
 
 import com.amr.project.model.entity.Item;
 
+import java.util.List;
+
 public interface ItemService extends ReadWriteService<Item, Long> {
+    @Override
+    void persist(Item item);
+
+    @Override
+    void update(Item item);
+
+    @Override
+    void delete(Item item);
+
+    @Override
+    List<Item> getAll();
+
+    @Override
+    Item getByKey(Long id);
+
+    @Override
+    Item getByName(String name);
 }
