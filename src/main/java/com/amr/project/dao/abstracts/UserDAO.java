d4dec8373d72b4de7d2377875d868fd6d7fd7f31
vladimirm <naame2@mail.ru>
2021-08-04 20:10:32 +0300

    1

--- src/main/java/com/amr/project/dao/abstracts/UserDAO.java
+++
@@ -1,7 +0,0 @@
-package com.amr.project.dao.abstracts;
-
-import com.amr.project.model.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDAO extends ReadWriteDAO<User, Long> {
-}
