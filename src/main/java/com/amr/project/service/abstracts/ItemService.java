b7200b16e0a9f773deb6289454fb3517a9f6659e
Nadin <programmer_n@bk.ru>
2021-08-05 01:41:25 +0500

    удалила переопределенные методы в Item DAO, Servise

--- src/main/java/com/amr/project/service/abstracts/ItemService.java
+++ src/main/java/com/amr/project/service/abstracts/ItemService.java
@@ -3,24 +3,6 @@ package com.amr.project.service.abstracts;
 
 import com.amr.project.model.entity.Item;
 
-import java.util.List;
-
 public interface ItemService extends ReadWriteService<Item, Long> {
-    @Override
-    void persist(Item item);
 
-    @Override
-    void update(Item item);
-
-    @Override
-    void delete(Item item);
-
-    @Override
-    List<Item> getAll();
-
-    @Override
-    Item getByKey(Long id);
-
-    @Override
-    Item getByName(String name);
 }
--- src/main/java/com/amr/project/service/impl/ItemServiceImpl.java
+++ src/main/java/com/amr/project/service/impl/ItemServiceImpl.java
@@ -1,15 +1,10 @@
 package com.amr.project.service.impl;
 
-
-import com.amr.project.dao.abstracts.ItemDao;
-import com.amr.project.dao.abstracts.ReadWriteDAO;
-import com.amr.project.dao.impl.ItemDaoImpl;
 import com.amr.project.model.entity.Item;
 import com.amr.project.service.abstracts.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class ItemServiceImpl extends ReadWriteServiceImpl<Item, Long> implements ItemService {
@@ -20,35 +15,5 @@ public class ItemServiceImpl extends ReadWriteServiceImpl<Item, Long> implements
     public ItemServiceImpl(ReadWriteDAO<Item, Long> readWriteDAO, ItemDao itemDao) {
         super(readWriteDAO);
         this.itemDao = itemDao;
-    }
-
-    @Override
-    public void persist(Item item) {
-        itemDao.persist(item);
-    }
-
-    @Override
-    public void update(Item item) {
-        itemDao.update(item);
-    }
-
-    @Override
-    public void delete(Item item) {
-        itemDao.delete(item);
-    }
-
-    @Override
-    public Item getByKey(Long id) {
-        return itemDao.getByKey(id);
-    }
-
-    @Override
-    public List<Item> getAll() {
-        return itemDao.getAll();
-    }
-
-    @Override
-    public Item getByName(String name) {
-        return itemDao.getByName(name);
     }
 }
