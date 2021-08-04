bccf0ac43a3aa14879f1f9317a6266b0203152e9
Denis Sharov <denissharov197@gmail.com>
2021-08-04 23:05:04 +0300

    Add service and repository annotation

--- src/main/java/com/amr/project/dao/impl/ReviewDaoImpl.java
+++ src/main/java/com/amr/project/dao/impl/ReviewDaoImpl.java
@@ -2,6 +2,8 @@ package com.amr.project.dao.impl;
 
 import com.amr.project.dao.abstracts.ReviewDao;
 import com.amr.project.model.entity.Review;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ReviewDaoImpl extends ReadWriteDAOImpl<Review, Long> implements ReviewDao {
 }
--- src/main/java/com/amr/project/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/amr/project/service/impl/ReviewServiceImpl.java
@@ -3,7 +3,9 @@ package com.amr.project.service.impl;
 import com.amr.project.dao.abstracts.ReviewDao;
 import com.amr.project.model.entity.Review;
 import com.amr.project.service.abstracts.ReviewService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ReviewServiceImpl extends ReadWriteServiceImpl<Review, Long> implements ReviewService {
     private final ReviewDao reviewDao;
 
