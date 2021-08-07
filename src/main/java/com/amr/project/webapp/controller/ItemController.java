8f2c953d5ff1707a90bd2a89a2780ed9d6a667d2
Yannh011 <yannh0101@gmail.com>
2021-08-07 12:31:44 +0300

    review_page1

--- src/main/java/com/amr/project/webapp/controller/ItemController.java
+++ src/main/java/com/amr/project/webapp/controller/ItemController.java
@@ -2,6 +2,7 @@ package com.amr.project.webapp.controller;
 
 import com.amr.project.model.entity.Review;
 import com.amr.project.service.abstracts.ItemService;
+import com.amr.project.service.abstracts.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,26 +17,32 @@ import java.util.Date;
 public class ItemController {
 
     private ItemService itemService;
+    private ReviewService reviewService;
 
-    public ItemController(ItemService itemService) {
+    @Autowired
+    public ItemController(ItemService itemService, ReviewService reviewService) {
         this.itemService = itemService;
+        this.reviewService = reviewService;
     }
-
 
     @GetMapping("/product/{id}")
     public String getItemPage(@PathVariable("id") Long id, Model model, @ModelAttribute("review") Review review) {
         model.addAttribute("item", itemService.getByKey(id));
         model.addAttribute("reviews", itemService.getByKey(id).getReviews());
+        model.addAttribute("newReview", new Review());
         return "product_page";
     }
 
-    @PostMapping("/product/{id}")
-    public String addReview(@PathVariable("id") Long id, @ModelAttribute("review") Review review, @ModelAttribute("rating") int rating) {
-        Collection<Review> reviews = itemService.getByKey(id).getReviews();
-        review.setRating(rating);
-        review.setDate(new java.sql.Date(System.currentTimeMillis()));
-        reviews.add(review);
-        itemService.getByKey(id).setReviews(reviews);
+    @PostMapping("/newReview")
+    public String addReview(@ModelAttribute("review") Review review) {
+
+//        Collection<Review> reviews = itemService.getByKey(id).getReviews();
+//        review.setDate(new java.sql.Date(System.currentTimeMillis()));
+//        reviews.add(review);
+//        itemService.getByKey(id).setReviews(reviews);
+        System.out.println(review.toString());
+        reviewService.persist(review);
+
         return "redirect:/home";
     }
 }
