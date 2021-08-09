ae484b2019bda5c4544a06644393dca35507da6d
ElenaLyubushkina <elyubushkina@yandex.ru>
2021-08-09 14:30:16 +0300

    admin pannel_v1

--- src/main/java/com/amr/project/ProjectApplication.java
+++ src/main/java/com/amr/project/ProjectApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
 
-@SpringBootApplication(exclude = {SessionAutoConfiguration.class
-        ,org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
-})
+@SpringBootApplication(exclude = {SessionAutoConfiguration.class})
 public class ProjectApplication {
 
     public static void main(String[] args) {
