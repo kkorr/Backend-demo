dcce8976b2d8d7db6b0a389e28580a36cdf6440d
PetrShed <petrshed@gmail.com>
2021-08-09 18:48:10 +0300

    pre-merge

--- src/main/java/com/amr/project/webapp/rest_controller/FavoriteRestController.java
+++ src/main/java/com/amr/project/webapp/rest_controller/FavoriteRestController.java
@@ -4,12 +4,11 @@ import com.amr.project.converter.ItemMapper;
 import com.amr.project.converter.ShopMapper;
 import com.amr.project.model.dto.CartItemDto;
 import com.amr.project.model.dto.ItemDto;
-import com.amr.project.model.entity.CartItem;
-import com.amr.project.model.entity.Favorite;
-import com.amr.project.model.entity.Item;
-import com.amr.project.model.entity.User;
+import com.amr.project.model.dto.ShopDto;
+import com.amr.project.model.entity.*;
 import com.amr.project.service.abstracts.FavoriteService;
 import com.amr.project.service.abstracts.ItemService;
+import com.amr.project.service.abstracts.ShopService;
 import com.amr.project.service.abstracts.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,22 +35,24 @@ public class FavoriteRestController {
     private final UserService userService;
     private final ItemService itemService;
     private final FavoriteService favoriteService;
+    private final ShopService shopService;
     private final ItemMapper itemMapper;
     private final ShopMapper shopMapper;
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    public FavoriteRestController(UserService userService, ItemService itemService,
-                                  ItemMapper itemMapper, FavoriteService favoriteService, ShopMapper shopMapper) {
+    public FavoriteRestController(UserService userService, ItemService itemService, ItemMapper itemMapper,
+                                  FavoriteService favoriteService, ShopService shopService, ShopMapper shopMapper) {
         this.userService = userService;
         this.itemService = itemService;
         this.favoriteService = favoriteService;
+        this.shopService = shopService;
         this.itemMapper = itemMapper;
         this.shopMapper = shopMapper;
     }
 
     @Transactional
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/items", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<ItemDto>> getFavoriteItems() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if(authentication == null || authentication instanceof AnonymousAuthenticationToken) {
@@ -59,21 +60,66 @@ public class FavoriteRestController {
         }
         User user = userService.findByUsername(authentication.getName()).get();
         Optional<Favorite> optFavorites = favoriteService.findByUser(user);
-        Collection<Item> items = null;
+        Collection<Item> items;
         if (optFavorites.isPresent()) {
             Favorite favorites = optFavorites.get();
             items = favorites.getItems();
-            LOGGER.info(String.format("Пользователь с id %d успешно получил список избранных товаров в корзине", user.getId()));
+            LOGGER.info(String.format("Пользователь с id %d успешно получил список избранных товаров", user.getId()));
+            List<ItemDto> itemsDto = items.stream().map(i -> itemMapper.itemToItemDto(i)).collect(Collectors.toList());
+            return ResponseEntity.ok(itemsDto);
         }
-        List<ItemDto> itemsDto = items.stream().map(i -> itemMapper.itemToItemDto(i)).collect(Collectors.toList());
-        return ResponseEntity.ok(itemsDto);
+        return ResponseEntity.ok(null);
     }
 
     @Transactional
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/items/delete/{id}")
     public ResponseEntity<Void> deleteFavoriteItem(@PathVariable Long id) {
-        favoriteService.deleteByKeyCascadeIgnore(id);
+        Item item = itemService.getByKey(id);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            throw new AccessDeniedException("Вам нужно авторизоваться для доступа к избранному");
+        }
+        User user = userService.findByUsername(authentication.getName()).get();
+        favoriteService.findByUser(user).ifPresent(favorite -> {
+            favorite.getItems().remove(item);
+            LOGGER.info(String.format("Пользователь с id %d успешно удалил товар с id %d из избранного", user.getId(), item.getId()));
+        });
         return ResponseEntity.ok().body(null);
     }
 
+    @Transactional
+    @GetMapping(value = "/shops", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ShopDto>> getFavoriteShops() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            throw new AccessDeniedException("Вам нужно авторизоваться для доступа к избранному");
+        }
+        User user = userService.findByUsername(authentication.getName()).get();
+        Optional<Favorite> optFavorites = favoriteService.findByUser(user);
+        Collection<Shop> shops;
+        if (optFavorites.isPresent()) {
+            Favorite favorites = optFavorites.get();
+            shops = favorites.getShops();
+            LOGGER.info(String.format("Пользователь с id %d успешно получил список избранных магазинов", user.getId()));
+            List<ShopDto> shopsDto = shops.stream().map(s -> shopMapper.shopToShopDto(s)).collect(Collectors.toList());
+            return ResponseEntity.ok(shopsDto);
+        }
+        return ResponseEntity.ok(null);
+    }
+
+    @Transactional
+    @DeleteMapping("/shops/delete/{id}")
+    public ResponseEntity<Void> deleteFavoriteShop(@PathVariable Long id) {
+        Shop shop = shopService.getByKey(id);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            throw new AccessDeniedException("Вам нужно авторизоваться для доступа к избранному");
+        }
+        User user = userService.findByUsername(authentication.getName()).get();
+        favoriteService.findByUser(user).ifPresent(favorite -> {
+            favorite.getShops().remove(shop);
+            LOGGER.info(String.format("Пользователь с id %d успешно удалил  магазин с id %d из избранного", user.getId(), shop.getId()));
+        });
+        return ResponseEntity.ok().body(null);
+    }
 }
--- src/main/resources/static/js/favorites.js
+++ src/main/resources/static/js/favorites.js
@@ -1,12 +1,25 @@
 async function getFavoriteItems() {
-    const response = await fetch("/api/favorites")
+    const response = await fetch("/api/favorites/items")
     const data = await response.json();
     console.log(data)
     if(document.getElementById("favoriteItems")) {
         data.forEach(item=> insertFavoriteItemRow(item))
     }
+
+}
+async function getFavoriteShops() {
+    const response = await fetch("/api/favorites/shops")
+    const data = await response.json();
+    console.log(data)
+    if(document.getElementById("favoriteShops")) {
+        data.forEach(shop=> insertFavoriteShopRow(shop))
+    }
 }
 getFavoriteItems();
+getFavoriteShops();
+
+document.getElementById("cartFromFavoritesButton").addEventListener('click',
+    addToCartFromFavorites());
 
 function insertFavoriteItemRow(item){
     let i = {
@@ -17,6 +30,7 @@ function insertFavoriteItemRow(item){
         categories: item.categories,
         images: item.images,
         description: item.description,
+        shopId: item.shopId
     };
     document.querySelector('#favoriteItems').insertAdjacentHTML('beforeend', `
     <div class="container" id="favoriteItem${i.id}" name="favoriteItemRow">
@@ -44,15 +58,81 @@ function insertFavoriteItemRow(item){
         <div class="row m-1">&nbsp;</div>
     </div>
     `)
+
+}
+function insertFavoriteShopRow(shop) {
+    let s = {
+        id: shop.id,
+        name: shop.name,
+        logo: shop.logo,
+        description: shop.description,
+    };
+    document.querySelector('#favoriteShops').insertAdjacentHTML('beforeend', `
+    <div class="container" id="favoriteShop${s.id}" name="favoriteShopRow">
+        <div class="row">
+            <h5>${s.name}</h5>
+        </div>
+        <div class="row">
+            <div class="col-1">
+                <input type="checkbox" id="checkBox${s.id}"> 
+            </div>
+            <div class="col-4">
+              <img src="${s.logo}" class="img-fluid"/>
+            </div>
+            <div class="col-6">
+                <p>${s.description}</p>
+            </div>
+            <div class="col-1">
+                <a href="#" onclick="collapseFavoriteShop(${s.id}); deleteFavoriteShop(${s.id})"><i class="fas fa-minus-circle" style="color: red" onclick=""></i></a>
+            </div>
+        </div>
+        <div class="row m-1">&nbsp;</div>
+    </div>
+    `)
 }
 
+function collapseFavoriteShop(id){
+    let idHTML = "favoriteShop"+id;
+    document.getElementById(idHTML).remove();
+}
+async function deleteFavoriteShop(id){
+    let url = new URL("http://localhost:8888/api/favorites/shops/delete/"+id);
+
+    const response = await fetch(url, {
+        headers: { "Content-Type": "application/json; charset=utf-8" },
+        method: 'DELETE'
+    })
+}
 function collapseFavoriteItem(id){
     let idHTML = "favoriteItem"+id;
     document.getElementById(idHTML).remove();
 }
 async function deleteFavoriteItem(id){
+    let url = new URL("http://localhost:8888/api/favorites/items/delete/"+id);
 
+    const response = await fetch(url, {
+        headers: { "Content-Type": "application/json; charset=utf-8" },
+        method: 'DELETE'
+    })
 }
-function addToCartFromFavorites() {
 
+async function addToCartFromFavorites() {
+    let quant = document.getElementById("itemQuant").value;
+    let url = new URL("http://localhost:8888/api/cart/add")
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify({
+            quantity: quant,
+            item: {
+                id: itemId
+            },
+            shop: {
+                id: shopId
+            },
+        })
+    })
 }
