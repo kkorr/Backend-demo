b67703e353e937d25e60297eb29f997126544976
Alexandr Kashevarov <landing.page.creature@gmail.com>
2021-08-12 20:50:09 +0500

    orderservice

--- src/main/java/com/amr/project/service/impl/OrderServiceImpl.java
+++ src/main/java/com/amr/project/service/impl/OrderServiceImpl.java
@@ -1,9 +1,18 @@
 package com.amr.project.service.impl;
 
+import com.amr.project.dao.abstracts.OrderDao;
 import com.amr.project.model.entity.Order;
 import com.amr.project.service.abstracts.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl extends ReadWriteServiceImpl<Order, Long> implements OrderService {
+    private OrderDao orderDao;
+
+    @Autowired
+    public OrderServiceImpl(OrderDao orderDao) {
+        super(orderDao);
+        this.orderDao = orderDao;
+    }
 }
