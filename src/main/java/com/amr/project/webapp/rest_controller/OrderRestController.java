4f323ddadb00d640209df592ffd98c52916231a7
Nadin <programmer_n@bk.ru>
2021-08-12 23:32:22 +0500

    OrderRestController

--- src/main/java/com/amr/project/webapp/rest_controller/OrderRestController.java
+++ src/main/java/com/amr/project/webapp/rest_controller/OrderRestController.java
@@ -1,8 +1,12 @@
 package com.amr.project.webapp.rest_controller;
 
+import com.amr.project.converter.OrderMapper;
 import com.amr.project.model.dto.OrderDto;
+import com.amr.project.model.entity.Order;
+import com.amr.project.service.abstracts.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,38 +16,41 @@ import org.springframework.web.bind.annotation.*;
 public class OrderRestController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+    private final OrderService orderService;
+    private final OrderMapper orderMapper;
 
+    @Autowired
+    public OrderRestController(OrderService orderService, OrderMapper orderMapper) {
+        this.orderService = orderService;
+        this.orderMapper = orderMapper;
+    }
 
     @GetMapping("/{orderId}")
-    public ResponseEntity<OrderDto> getOrder(@PathVariable("orderId") String orderId) {
-        return null;
+    public ResponseEntity<OrderDto> getOrder(@PathVariable("orderId") Long orderId) {
+        return new ResponseEntity<>(orderMapper.orderToDto(orderService.getByKey(orderId)),
+                HttpStatus.OK);
     }
 
     @PutMapping("/save")
     public ResponseEntity<Long> saveOrder(@RequestBody OrderDto orderDto) {
-        System.out.println("orderDto" + orderDto);
 
-//        Order order = orderMapper.orderDtoToOrder(orderDto);
-//        orderService.update(order);
+        Order order = orderMapper.dtoToOrder(orderDto);
+        orderService.update(order);
+        LOGGER.info(String.format("Пользователь изменил данные заказа", orderDto));
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/add")
     public ResponseEntity<Long> addOrder(@RequestBody OrderDto orderDto) {
-//        String[] strings = {};
-//        orderDto.setImages(strings);
-//        orderDto.setReviews(strings);
-//        System.out.println("orderDto1" + orderDto);
-//
-//        Orde order = orderMapper.orderDtoToOrder(orderDto);
-//        orderService.persist(order);
-
+        Order order = orderMapper.dtoToOrder(orderDto);
+        orderService.persist(order);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<OrderDto> delete(@PathVariable("id") Long id) {
-//        orderService.deleteByKeyCascadeIgnore(id);
+        orderService.deleteByKeyCascadeIgnore(id);
+        LOGGER.info(String.format("Пользователь удалил заказ с id %d", orderService.getByKey(id)));
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
