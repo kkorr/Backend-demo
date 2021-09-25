65df094809b764a053c96b90dcf6c94dadd1e0d7
Svyatoslav Pichcar <sv.pich@mail.ru>
2021-09-25 15:38:01 +0900

    добавил навБар на страниу Магазны

--- src/main/java/com/amr/project/webapp/controller/ShopController.java
+++ src/main/java/com/amr/project/webapp/controller/ShopController.java
@@ -1,2 +1,13 @@
-package com.amr.project.webapp.controller;public class ShopController {
+package com.amr.project.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShopController {
+
+    @GetMapping("/shops")
+    public String cartAllShops() {
+        return "shops";
+    }
 }
