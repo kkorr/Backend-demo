fcb9db4c342c94c8017ab1b680f0ad910b86dd72
hoi_bj <d.besedin.0@ya.ru>
2021-09-30 12:39:20 +0300

    Added custom authentication provider.

--- src/main/java/com/amr/project/security/SecurityConfig.java
+++ src/main/java/com/amr/project/security/SecurityConfig.java
@@ -7,11 +7,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -23,15 +23,15 @@ import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userDetailsService;
     private final SuccessUserHandler successUserHandler;
     private final CustomOAuth2UserService oAuth2UserService;
     private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
     private final CustomWebAuthenticationDetailsSource authenticationDetailsSource;
+    private final CustomAuthenticationProvider customAuthenticationProvider;
 
     @Autowired
     public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        auth.authenticationProvider(authenticationProvider(customAuthenticationProvider));
     }
 
     @Override
@@ -75,5 +75,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public SpringSecurityDialect securityDialect() {
         return new SpringSecurityDialect();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(CustomAuthenticationProvider authenticationProvider) {
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
     }
 }
