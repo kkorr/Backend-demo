10367e705c9823f762505a725ba14387c4fa7412
AndySpinko <spdale016@gmail.com>
2021-10-01 17:31:11 +0700

    added navbar in favorites

--- src/main/java/com/amr/project/webapp/controller/FavoriteController.java
+++ src/main/java/com/amr/project/webapp/controller/FavoriteController.java
@@ -1,13 +1,35 @@
 package com.amr.project.webapp.controller;
 
+import com.amr.project.model.entity.User;
+import com.amr.project.service.abstracts.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Controller
 public class FavoriteController {
 
+    private final UserService userService;
+
+    @Autowired
+    public FavoriteController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/favorites")
-    public String cartPage() {
+    public String getProfile(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Optional<User> user = userService.findByUsername(authentication.getName());
+        if (authentication.isAuthenticated() && user.isPresent()) {
+            model.addAttribute("user", user.get());
+        } else {
+            model.addAttribute("user", new User());
+        }
         return "favorites";
     }
 }
