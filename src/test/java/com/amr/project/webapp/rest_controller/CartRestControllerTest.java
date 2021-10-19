049eebf151d69dda2342cb179b4e414c0ac8fac6
Svyatoslav Pichcar <sv.pich@mail.ru>
2021-10-19 14:35:18 +0900

    first test

--- src/test/java/com/amr/project/webapp/rest_controller/CartRestControllerTest.java
+++ src/test/java/com/amr/project/webapp/rest_controller/CartRestControllerTest.java
@@ -1,13 +1,38 @@
 package com.amr.project.webapp.rest_controller;
 
 import com.amr.project.AbstractIntegrationTest;
+import com.amr.project.model.dto.CartItemDto;
+import com.amr.project.model.dto.ItemDto;
+import com.amr.project.model.dto.ShopDto;
+import com.amr.project.model.entity.CartItem;
+import com.amr.project.model.entity.Item;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.Request;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,8 +41,33 @@ import org.springframework.test.context.junit4.SpringRunner;
 @WithUserDetails(value = "petr")
 public class CartRestControllerTest extends AbstractIntegrationTest {
 
-    @Test
-    void addItemToCart() {
+    @Autowired
+    private CartRestController cartRestController;
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void addItemToCart2() throws Exception {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setId(1L);
+
+        ShopDto shopDto = new ShopDto();
+        shopDto.setId(1L);
+
+        CartItemDto cartItemDto = new CartItemDto();
+        cartItemDto.setQuantity(1);
+        cartItemDto.setItem(itemDto);
+        cartItemDto.setShop(shopDto);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson=ow.writeValueAsString(cartItemDto);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/cart/add").contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
