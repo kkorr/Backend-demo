73e5e7fb93abfb5b8a8fff60f7e6a3b857736e88
Александр Моисеев <sobergrim@gmail.com>
2021-11-06 12:40:08 +0300

    v 1.11

--- src/main/java/com/amr/project/model/dto/FavoriteDto.java
+++ src/main/java/com/amr/project/model/dto/FavoriteDto.java
@@ -1,11 +1,25 @@
 package com.amr.project.model.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.amr.project.model.entity.Favorite;
+import com.amr.project.model.entity.Item;
+import com.amr.project.model.entity.Shop;
+import com.amr.project.model.entity.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
-@NoArgsConstructor
+import javax.persistence.*;
+import java.util.Collection;
+
 @Getter
 @Setter
 public class FavoriteDto {
+
+    public FavoriteDto (Favorite fav) {
+        this.items = fav.getItems();
+        this.shops = fav.getShops();
+    }
+
+    private Collection<Shop> shops;
+
+    private Collection<Item> items;
 }
--- src/main/java/com/amr/project/webapp/controller/FavoriteController.java
+++ src/main/java/com/amr/project/webapp/controller/FavoriteController.java
@@ -1,5 +1,7 @@
 package com.amr.project.webapp.controller;
 
+import com.amr.project.model.dto.FavoriteDto;
+import com.amr.project.model.entity.Favorite;
 import com.amr.project.model.entity.User;
 import com.amr.project.service.abstracts.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
@@ -32,4 +37,14 @@ public class FavoriteController {
         }
         return "favorites";
     }
+
+    @PostMapping("/favorites")
+    public FavoriteDto getProfilePost(@RequestBody String userName) {
+        //todo: контроль входного параметра userName. Вдруг там строка с хакерским кодом
+        Optional<User> user = userService.findByUsername(userName);
+        Favorite fav = user.map(User::getFavorite).orElse(new Favorite());
+
+        return new FavoriteDto(fav);
+    }
+
 }
